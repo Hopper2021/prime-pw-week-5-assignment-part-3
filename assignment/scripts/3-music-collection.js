86e57c840143374b7805645df037eac9795695a0
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-08-12 15:25:05 -0500

    Changed addToCollection function, commented out bad code, and added 6 albums

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,16 +2,38 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
+// 1st attempt: The below function was adding the three parameters as their own values
+// function addToCollection( title, artist, yearPublished ) {
+//     collection.push( title, artist, yearPublished );
+//     return true;
+//   }
+
+// This function actually creates an object and assigns the input parameters as properties.
+// There is a discussion on this in Slack that helped me understand
 function addToCollection( title, artist, yearPublished ) {
-    collection.push( title, artist, yearPublished );
-    return true;
+    let album = { title, artist, yearPublished };
+    collection.push( album ) ;
+    return album;
   }
 
-album0 = {
-  title: 'Ultra Blue',
-  artist: 'Hikaru Utada',
-  yearPublished: 2006
-}
+// Misread instructions and created an album in the wrong place
+// const album0 = {
+//   title: 'Ultra Blue',
+//   artist: 'Hikaru Utada',
+//   yearPublished: 2006
+// };
 
-console.log( 'Adding album0 to collection (expects true):', addToCollection( album0 ) );
+// testing addToCollection function, added album incorrectly
+// console.log( 'Adding album to collection (expects album properties):', addToCollection( 'Ultra Blue', 'Hikaru Utada', 2006 ) );
 console.log( `My collection now contains: ${collection}` );
+
+// Setting the output of addToCollection as an object that can be called
+let album = console.log( addToCollection() );
+
+// Adding 5 more albums to get the 6 to test addToCollection
+console.log( 'Added:', addToCollection( 'New Eyes', 'Clean Bandit', 2014 ), album );
+console.log( 'Added:', addToCollection( 'Riot!', 'Paramore', 2007 ) );
+console.log( 'Added:', addToCollection( 'After Hours', 'The Weeknd', 2020 ) );
+console.log( 'Added:', addToCollection( 'Professional Rapper', 'Lil Dicky', 2015 ) );
+console.log( 'Added:', addToCollection( 'Because the Internet', 'Childish Gambino', 2013 ) );
+console.log( 'Added:', addToCollection( 'Ultra Blue', 'Hikaru Utada', 2006 ) );
