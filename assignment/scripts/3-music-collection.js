7a468af69376a868f7f84ecbf103ee3dd2b15056
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-08-12 10:11:57 -0500

    Attempting to log collection with pushed album. Unsuccessful

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -3,5 +3,15 @@ console.log('***** Music Collection *****')
 let collection = [];
 
 function addToCollection( title, artist, yearPublished ) {
-  return true;
+    collection.push( title, artist, yearPublished );
+    return true;
+  }
+
+album0 = {
+  title: 'Ultra Blue',
+  artist: 'Hikaru Utada',
+  yearPublished: 2006
 }
+
+console.log( 'Adding album0 to collection (expects true):', addToCollection( album0 ) );
+console.log( `My collection now contains: ${collection}` );
