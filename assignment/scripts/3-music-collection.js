0b0d2814f75d2d5ac96cc169318282f67c284a84
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-08-15 10:36:09 -0500

    Fixed logic error of missing ( i < ) in for loop and added return collection. Search function now successful

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,15 +2,6 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
-// I'll try to leave as many of my mistakes commented out to show the steps I took
-// and how I got to my answer. Please let me know if you would like me to change this.
-
-// 1st attempt: The below function was adding the three parameters as their own values
-// function addToCollection( title, artist, yearPublished ) {
-//     collection.push( title, artist, yearPublished );
-//     return true;
-//   }
-
 // This function actually creates an object and assigns the input parameters as properties.
 // There is a discussion on this in Slack that helped me understand
 function addToCollection( title, artist, yearPublished ) {
@@ -18,22 +9,6 @@ function addToCollection( title, artist, yearPublished ) {
     collection.push( album ) ;
     return album;
   }
-
-  // Below, I tried to capture the output in a different value, but I realized that
-  // pulling it out from the array its already being pushed to sounds like
-  // the better answer.
-  // let album = addToCollection();
-
-// Misread instructions and created an album in the wrong place
-// const album0 = {
-//   title: 'Ultra Blue',
-//   artist: 'Hikaru Utada',
-//   yearPublished: 2006
-// };
-
-// testing addToCollection function, added album incorrectly
-// console.log( 'Adding album to collection (expects album properties):', addToCollection( 'Ultra Blue', 'Hikaru Utada', 2006 ) );
-// console.log( `My collection now contains: ${collection}` );
 
 // Adding 6 albums to get the 6 to test addToCollection
 console.log( 'Added the following albums to collection array:' );
@@ -49,7 +24,7 @@ console.log( collection );
 // 1st attempt at showCollection properties
 function showCollection( collection ) {
   console.log( 'Number of albums in the collection:', collection.length );
-  for ( i=0; i<collection.length; i++ ){
+  for (let i=0; i<collection.length; i++ ){
     console.log( `${collection[i].title} by ${collection[i].artist}, published in ${collection[i].yearPublished}` );
   } // end for loop
 } // end showCollection
@@ -58,7 +33,7 @@ showCollection( collection ); // test showCollection
 
 function findByArtist ( artist ) {
   let result = [];
-  for ( i=0; i<collection.length; i++ ) {
+  for (let i=0; i<collection.length; i++ ) {
     if ( artist === collection[i].artist ) {
       result.push( artist ); // push input parameter into array
     } // end if statement
@@ -68,17 +43,16 @@ function findByArtist ( artist ) {
 console.log( 'Find artist (expects Clean Bandit twice):', findByArtist ( 'Clean Bandit' ) ); // testing findByArtist
 console.log( 'Find artist (expects empty array):',findByArtist ( 'UVERworld' ) ); // testing findByArtist
 
-function search( searchArtist, searchYearPublished ) {
+function search( object ) {
   let result = [];
-  for ( i=0; collection.length; i++ ) {
-    if ( searchArtist === collection[i].artist && searchYearPublished === collection[i].yearPublished) {
+  for (let i=0; i<collection.length; i++ ) {
+    if ( object.artist === collection[i].artist && object.year === collection[i].yearPublished ) {
       result.push( object );
-    } else if (searchArtist != collection[i].artist && searchYearPublished != collection[i].yearPublished) {
-      return collection;
-    }
-  } return result;
-}
+      return result; // jumps out of loop if OBJECT is found
+    } // end if statement
+  } return collection; // if OBJECT is not found, returns collection
+} // end search function
 
-console.log( 'Find Ray Charles (expects collection):', search({ artist: 'Ray Charles', year: 1957 }));
-console.log( 'Added:', addToCollection ( '', 'Ray Charles', 1957 ) ); // adding to test search function
-console.log( 'Find Ray Charles (expects Ray Charles, 1957):', search({ artist: 'Ray Charles', year: 1957 }));
+console.log( 'Find Ray Charles (expects collection):', search({ artist: 'Ray Charles', year: 1957 }) );
+console.log( 'Added:', addToCollection ( 'Some song', 'Ray Charles', 1957 ) ); // adding to test search
+console.log( 'Find Ray Charles (expects Ray Charles, 1957):', search({ artist: 'Ray Charles', year: 1957 }) );
