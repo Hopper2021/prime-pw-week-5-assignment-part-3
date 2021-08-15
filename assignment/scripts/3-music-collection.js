277f984c528b1f6361787cba6e21506424905ad2
Jessica Buckwalter <jessica.a.buckwalter@gmail.com>
2021-08-15 11:29:33 -0500

    Added tracks to addToCollection function and logged a track to test. Successful

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -2,17 +2,17 @@ console.log('***** Music Collection *****')
 
 let collection = [];
 
-// This function actually creates an object and assigns the input parameters as properties.
+// This function creates an object and assigns the input parameters as properties.
 // There is a discussion on this in Slack that helped me understand
-function addToCollection( title, artist, yearPublished ) {
-    let album = { title, artist, yearPublished };
+function addToCollection( title, artist, yearPublished, tracks = ["trackName", "duration"] ) {
+    let album = { title, artist, yearPublished, tracks };
     collection.push( album ) ;
     return album;
   }
 
 // Adding 6 albums to get the 6 to test addToCollection
 console.log( 'Added the following albums to collection array:' );
-console.log( 'Added:', addToCollection( 'New Eyes', 'Clean Bandit', 2014 ) );
+console.log( 'Added:', addToCollection( 'New Eyes', 'Clean Bandit', 2014, ['Real Love', 3.35], ['Higher', 2.54] ) );
 console.log( 'Added:', addToCollection( 'What is love?', 'Clean Bandit', 2007 ) );
 console.log( 'Added:', addToCollection( 'After Hours', 'The Weeknd', 2020 ) );
 console.log( 'Added:', addToCollection( 'Professional Rapper', 'Lil Dicky', 2015 ) );
@@ -20,12 +20,14 @@ console.log( 'Added:', addToCollection( 'Because the Internet', 'Childish Gambin
 console.log( 'Added:', addToCollection( 'Ultra Blue', 'Hikaru Utada', 2006 ) );
 console.log( collection );
 
+
 // showCollection function, meant to be reusable to show any Collection
 // 1st attempt at showCollection properties
 function showCollection( collection ) {
   console.log( 'Number of albums in the collection:', collection.length );
   for (let i=0; i<collection.length; i++ ){
-    console.log( `${collection[i].title} by ${collection[i].artist}, published in ${collection[i].yearPublished}` );
+    console.log( `${collection[i].title} by ${collection[i].artist}, published in ${collection[i].yearPublished} \n
+      NAME: ${collection[i].trackName}: DURATION: ${collection[i].duration}` );
   } // end for loop
 } // end showCollection
 
@@ -43,7 +45,7 @@ function findByArtist ( artist ) {
 console.log( 'Find artist (expects Clean Bandit twice):', findByArtist ( 'Clean Bandit' ) ); // testing findByArtist
 console.log( 'Find artist (expects empty array):',findByArtist ( 'UVERworld' ) ); // testing findByArtist
 
-function search( object ) {
+function search( object, trackName ) {
   let result = [];
   for (let i=0; i<collection.length; i++ ) {
     if ( object.artist === collection[i].artist && object.year === collection[i].yearPublished ) {
